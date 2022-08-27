b6c171871026b8295fbaafbe6ac800007e0f0c7c
walthercio Almeida <suaagenciainicial@gmail.com>
2022-08-27 04:18:55 -0300

    adiciona requisito 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { user, wallet } = this.props;
     return (
-      <div>Header</div>
+      <div>
+        <h4 data-testid="email-field">{user.email}</h4>
+        <h4 data-testid="total-field">{wallet.idToEdit}</h4>
+        <h4 data-testid="header-currency-field">BRL</h4>
+      </div>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    user: state.user,
+    wallet: state.wallet,
+  };
+}
+
+Header.propTypes = {
+  user: PropTypes.string,
+  wallet: PropTypes.string,
+
+}.isRequired;
+
+export default connect(mapStateToProps)(Header);
