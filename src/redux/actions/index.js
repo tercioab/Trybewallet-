92fe1cc536d308d838fc9a72478d9e4061249c04
walthercio Almeida <suaagenciainicial@gmail.com>
2022-08-27 19:01:19 -0300

    adicion

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -5,18 +5,33 @@ export function userAction(payload) {
   };
 }
 
-export function walletAction(payload) {
+function loading() {
   return {
-    type: 'WALLET',
-    payload,
+    type: 'FUNC',
+    payload: true,
+  };
+}
+
+function loadingFalse() {
+  return {
+    type: 'FUNC',
+    payload: false,
+  };
+}
+
+function currencies(wallet) {
+  return {
+    type: wallet,
   };
 }
 
 export function fetchEconomia() {
   return async (dispatch) => { // thunk declarado
-    dispatch(requestMovies());
+    dispatch(loading());
     const response = await fetch('https://economia.awesomeapi.com.br/json/all');
     const data = await response.json();
-    return dispatch(receiveCoins(data));
+    delete data.USDT;
+    dispatch(currencies(Object.keys(data)));
+    dispatch(loadingFalse);
   };
 }
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -7,10 +7,10 @@ const INITIAL_STATE = {
 
 function wallet(state = INITIAL_STATE, action) {
   switch (action.type) {
+  case 'WALLET':
+    return { ...state, loading: action.payload };
   case 'FUNC':
     return { ...state, currencies: action.payload };
-  case 'WALLET':
-    return { ...state, ...action.payload };
   default:
     return state;
   }
