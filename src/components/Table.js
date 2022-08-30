d0d3614a38971bf0c2aff91dbd26b75996c3800a
walthercio Almeida <suaagenciainicial@gmail.com>
2022-08-29 23:37:17 -0300

    depois de mil anos tentando passar descubro que o problema é na semantica

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <div>
-        <th>Descrição</th>
-        <th>Tag</th>
-        <th>Método de pagamento</th>
-        <th>Valor</th>
-        <th>Moeda</th>
-        <th>Câmbio utilizado</th>
-        <th>Valor convertido</th>
-        <th>Moeda de conversão</th>
-        <th>Editar/Excluir</th>
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses
+            .map(({ id, value, description, currency, method, tag, exchangeRates }) => (
+              <tr key={ id }>
+                <td>{description}</td>
+                <td>{tag}</td>
+                <td>{method}</td>
+                <td>
+                  {' '}
+                  {Number(value).toFixed(2)}
+                </td>
+                <td>{(exchangeRates[currency].name)}</td>
+                <td>{Number(exchangeRates[currency].ask).toFixed(2) }</td>
+                <td>{(exchangeRates[currency].ask * value).toFixed(2)}</td>
+                <td>Real</td>
+              </tr>))}
+        </tbody>
+      </table>
+
     );
   }
 }
 
-export default Table;
+function mapStateToProps(state) {
+  return {
+    expenses: state.wallet.expenses,
+  };
+}
+
+Table.propTypes = {
+  expenses: PropTypes.string,
+
+}.isRequired;
+
+export default connect(mapStateToProps)(Table);
