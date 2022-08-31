347dcd2bb7c179d55072a11ea7f91b5fe76af585
walthercio Almeida <suaagenciainicial@gmail.com>
2022-08-30 21:17:22 -0300

    adiciona requisito 5

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,9 +1,20 @@
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import renderWthRouterAndRedux from './helpers/renderWith';
 import App from '../App';
 
 describe('Teste componente "Login.js"', () => {
+  test('Verifica se ao entrar a rota está correta', () => {
+    const { history } = renderWthRouterAndRedux(<App />);
+    const email = screen.getByTestId('email-input');
+    const password = screen.getByTestId('password-input');
+    userEvent.type(email, 'suaagenciainicial@email.com');
+    userEvent.type(password, '123456');
+    const btn = screen.getByText('Entrar');
+    userEvent.click(btn);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/carteira');
+  });
   test('Verifica se existe o nome trybewallet', () => {
     renderWthRouterAndRedux(<App />);
     const login = screen.getByRole('heading', { name: /trybewallet/i });
@@ -14,6 +25,19 @@ describe('Teste componente "Login.js"', () => {
     renderWthRouterAndRedux(<App />);
     const email = screen.getByRole('textbox', { type: /email/i });
     expect(email).toBeInTheDocument();
+  });
+
+  test('Verifica se os inputs são validados', () => {
+    renderWthRouterAndRedux(<App />);
+    const email = screen.getByTestId('email-input');
+    const password = screen.getByTestId('password-input');
+    const btn = screen.getByText('Entrar');
+    userEvent.type(email, 'email');
+    userEvent.type(password, '12345');
+    expect(btn).toHaveAttribute('disabled');
+    userEvent.type(email, 'suaagenciainicial@email.com');
+    userEvent.type(password, '123456');
+    expect(btn).not.toHaveAttribute('disabled');
   });
 
   test('Verifica se existe o botão de entrar', () => {
