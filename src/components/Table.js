7a3bc3afe2dd3f2b5f3ba7e8a2218892bc55f147
walthercio Almeida <suaagenciainicial@gmail.com>
2022-08-31 16:43:10 -0300

    adiciona css e quebra varios requisitos ao mesmo tempo

--- src/components/Table.js
+++ src/components/Table.js
@@ -18,36 +18,46 @@ class Table extends Component {
     const { expenses } = this.props;
     return (
       <table>
-        <thead>
-          <tr className="description">
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-        </thead>
         <tbody>
           {expenses
             .map(({ id, value, description, currency, method, tag, exchangeRates }) => (
               <tr className="result" key={ id }>
-                <td>{description}</td>
-                <td>{tag}</td>
-                <td>{method}</td>
-                <td>
+                <td className="t1">
+                  <h3>Descrição</h3>
+                  {description}
+                </td>
+                <td className="t2">
+                  <h3>Tag</h3>
+                  {tag}
+                </td>
+                <td className="t3">
+                  <h3>Método de pagamento</h3>
+                  {method}
+                </td>
+                <td className="t4">
+                  <h3>Valor</h3>
                   {' '}
                   {Number(value).toFixed(2)}
                 </td>
-                <td>{(exchangeRates[currency].name)}</td>
-                <td>{Number(exchangeRates[currency].ask).toFixed(2) }</td>
-                <td>{(exchangeRates[currency].ask * value).toFixed(2)}</td>
-                <td>Real</td>
+                <td className="t5">
+                  <h3>Moeda</h3>
+                  {(exchangeRates[currency].name).split('/', 1)}
+                </td>
+                <td className="t6">
+                  <h3>Câmbio utilizado</h3>
+                  {Number(exchangeRates[currency].ask).toFixed(2) }
+                </td>
+                <td className="t7">
+                  <h3>Valor convertido</h3>
+                  {(exchangeRates[currency].ask * value).toFixed(2)}
+                </td>
+                <td className="t8">
+                  <h3>Moeda de conversão</h3>
+                  Real
+                </td>
                 <td>
                   <button
+                    className="t9"
                     type="button"
                     id={ id }
                     data-testid="delete-btn"
